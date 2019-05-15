b6958e73ae9ad032f0acfdc12d96c55f6914fce9
Samuel Septiano <sammuel.septiano@gmail.com>
2019-05-15 13:10:02 +0700

    Add files via upload
    initial commit

---
+++ instagram_config.php
@@ -0,0 +1,23 @@
+<?php
+
+$searchtag = array(
+	'superheroes',
+	'holiday',
+	'quotes', 
+	'health', 
+);
+
+
+$hello = array(	
+	'Hi',
+	'Hello',
+	'',
+);
+
+$praise = array(	
+	', awesome, dude.',
+	', this post is great!',
+);
+
+
+?>
